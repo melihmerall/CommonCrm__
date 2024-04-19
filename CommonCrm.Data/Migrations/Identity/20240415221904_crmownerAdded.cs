79e7119b2b14aaa6201c30f520c2f7bfd89a2fbd
melihmerall <melih16.meral@gmail.com>
2024-04-19 15:01:39 +0300

    customer operation added.

---
+++ CommonCrm.Data/Migrations/Identity/20240415221904_crmownerAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommonCrm.Data.Migrations.Identity
+{
+    /// <inheritdoc />
+    public partial class crmownerAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCrmOwner",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCrmOwner",
+                table: "AspNetUsers");
+        }
+    }
+}
